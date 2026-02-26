824c1659bac844feb09b602f93c34a8968319ddf
greywell71-cmd <greywell71@gmail.com>
2026-02-26 17:32:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -66,9 +66,9 @@ def analyze_market(symbol):
         last_prices[symbol] = current_price
 
         # --- СИГНАЛЫ RSI ---
-        if last_rsi > 70:
+        if last_rsi > 50:
             report += f"🚨 СИГНАЛ ПРОДАЖИ: {symbol}\nRSI: {round(last_rsi, 2)}\n"
-        elif last_rsi < 30:
+        elif last_rsi < 50:
             report += f"✅ СИГНАЛ ПОКУПКИ: {symbol}\nRSI: {round(last_rsi, 2)}\n"
 
         if report:
